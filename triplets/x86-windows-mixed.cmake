e048f8ba83eb598741b743bf7d11dd9e3a992bf5
DubbleClick <m@pyc.ac>
2024-11-11 12:20:44 +0700

    use vcpkg for more packages

---
+++ triplets/x86-windows-mixed.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CMAKE_SYSTEM_NAME windows)
+
+if(PORT MATCHES "dxsdk-d3dx")
+    set(VCPKG_CRT_LINKAGE dynamic)
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+    set(VCPKG_CRT_LINKAGE static)
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
